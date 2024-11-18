c8df0f4c4d273fad4f82ab504e1dbbc2e0bcce55
supakjack <supakjack@gmail.com>
2024-11-19 06:38:51 +0700

    migration

--- databases/migration/migration.go
+++ databases/migration/migration.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/supakjack/isekai-shop-api-tutorial/config"
 	"github.com/supakjack/isekai-shop-api-tutorial/databases"
+	"github.com/supakjack/isekai-shop-api-tutorial/entities"
+	"gorm.io/gorm"
 )
 
 func main() {
@@ -12,4 +14,44 @@ func main() {
 	db := databases.NewPostgresDatabase(conf.Database)
 
 	fmt.Println(db.ConnectionGetting())
+
+	tx := db.ConnectionGetting().Begin()
+
+	playerMigration(tx)
+	adminMigration(tx)
+	itemMigration(tx)
+	playerCoinMigration(tx)
+	inventoryMigration(tx)
+	purchaseHistoryMigration(tx)
+
+	tx.Commit()
+
+	if tx.Error != nil {
+		tx.Rollback()
+		panic(tx.Error)
+	}
+}
+
+func playerMigration(tx *gorm.DB) {
+	tx.Migrator().CreateTable(&entities.Player{})
+}
+
+func adminMigration(tx *gorm.DB) {
+	tx.Migrator().CreateTable(&entities.Admin{})
+}
+
+func itemMigration(tx *gorm.DB) {
+	tx.Migrator().CreateTable(&entities.Item{})
+}
+
+func playerCoinMigration(tx *gorm.DB) {
+	tx.Migrator().CreateTable(&entities.PlayerCoin{})
+}
+
+func inventoryMigration(tx *gorm.DB) {
+	tx.Migrator().CreateTable(&entities.Inventory{})
+}
+
+func purchaseHistoryMigration(tx *gorm.DB) {
+	tx.Migrator().CreateTable(&entities.PurchaseHistory{})
 }
