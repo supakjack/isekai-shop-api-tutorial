b3b3afb5ba868913200fe75d3a5fd1f9af460d5f
supakjack <supakjack@gmail.com>
2024-11-19 08:32:45 +0700

    add air and gracefully shutdown

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"github.com/supakjack/isekai-shop-api-tutorial/config"
+	"github.com/supakjack/isekai-shop-api-tutorial/databases"
+	"github.com/supakjack/isekai-shop-api-tutorial/server"
+)
 
+func main() {
+	conf := config.ConfigGetting()
+	db := databases.NewPostgresDatabase(conf.Database)
+	server := server.NewEchoServer(conf, db.ConnectionGetting())
+	server.Start()
 }
--- server/server.go
+++ server/server.go
@@ -1,1 +1,77 @@
 package server
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
+	"github.com/supakjack/isekai-shop-api-tutorial/config"
+	"gorm.io/gorm"
+)
+
+type echoServer struct {
+	app  *echo.Echo
+	db   *gorm.DB
+	conf *config.Config
+}
+
+var (
+	once   sync.Once
+	server *echoServer
+)
+
+func NewEchoServer(conf *config.Config, db *gorm.DB) *echoServer {
+	echoApp := echo.New()
+	echoApp.Logger.SetLevel(log.DEBUG)
+
+	once.Do(func() {
+		server = &echoServer{
+			app:  echoApp,
+			db:   db,
+			conf: conf,
+		}
+	})
+
+	return server
+}
+
+func (s *echoServer) Start() {
+	s.app.GET("/v1/health", s.healthCheck)
+
+	quitCh := make(chan os.Signal, 1)
+	signal.Notify(quitCh, syscall.SIGINT, syscall.SIGTERM)
+	go s.gracefullyShutdown(quitCh)
+
+	s.httpListening()
+}
+
+func (s *echoServer) httpListening() {
+	serveUrl := fmt.Sprintf(":%d", s.conf.Server.Port)
+
+	err := s.app.Start(serveUrl)
+	if err != nil && err != http.ErrServerClosed {
+		s.app.Logger.Fatalf("Error: %s", err.Error())
+	}
+}
+
+func (s *echoServer) gracefullyShutdown(quitCh <-chan os.Signal) {
+	ctx := context.Background()
+
+	<-quitCh
+	s.app.Logger.Infof("Shutting down server...")
+
+	err := s.app.Shutdown(ctx)
+	if err != nil {
+		s.app.Logger.Fatalf("Error: %s", err.Error())
+	}
+}
+
+func (s *echoServer) healthCheck(c echo.Context) error {
+	return c.String(http.StatusOK, "OK")
+}
