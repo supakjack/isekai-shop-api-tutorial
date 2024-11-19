cb8874ce8e8409160c61de65954de6d5727084ce
supakjack <supakjack@gmail.com>
2024-11-19 20:54:08 +0700

    middleware

--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
@@ -46,6 +47,7 @@ require (
 	golang.org/x/sync v0.9.0 // indirect
 	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.20.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- server/server.go
+++ server/server.go
@@ -8,8 +8,10 @@ import (
 	"os/signal"
 	"sync"
 	"syscall"
+	"time"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
 	"github.com/supakjack/isekai-shop-api-tutorial/config"
 	"gorm.io/gorm"
@@ -42,7 +44,20 @@ func NewEchoServer(conf *config.Config, db *gorm.DB) *echoServer {
 }
 
 func (s *echoServer) Start() {
+	corsMiddleware := getCORSMiddleware(s.conf.Server.AllowOrigins)
+	bodyLimitMiddleware := getBodyLimitMiddleware(s.conf.Server.BodyLimit)
+	timeOutMiddleware := getTimeOutMiddleware(s.conf.Server.Timeout)
+
+	s.app.Use(middleware.Recover())
+	s.app.Use(middleware.Logger())
+	s.app.Use(corsMiddleware)
+	s.app.Use(bodyLimitMiddleware)
+	s.app.Use(timeOutMiddleware)
+
 	s.app.GET("/v1/health", s.healthCheck)
+	s.app.GET("/v1/panic", func(c echo.Context) error {
+		panic("Panic!")
+	})
 
 	quitCh := make(chan os.Signal, 1)
 	signal.Notify(quitCh, syscall.SIGINT, syscall.SIGTERM)
@@ -74,4 +89,25 @@ func (s *echoServer) gracefullyShutdown(quitCh <-chan os.Signal) {
 
 func (s *echoServer) healthCheck(c echo.Context) error {
 	return c.String(http.StatusOK, "OK")
+}
+
+func getTimeOutMiddleware(timeout time.Duration) echo.MiddlewareFunc {
+	return middleware.TimeoutWithConfig(middleware.TimeoutConfig{
+		Skipper:      middleware.DefaultSkipper,
+		ErrorMessage: "Request Timeout",
+		Timeout:      timeout * time.Second,
+	})
+}
+
+func getCORSMiddleware(allowOrigins []string) echo.MiddlewareFunc {
+	return middleware.CORSWithConfig(middleware.CORSConfig{
+		Skipper:      middleware.DefaultSkipper,
+		AllowOrigins: allowOrigins,
+		AllowMethods: []string{echo.GET, echo.POST, echo.PUT, echo.PATCH, echo.DELETE},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	})
+}
+
+func getBodyLimitMiddleware(bodyLimit string) echo.MiddlewareFunc {
+	return middleware.BodyLimit(bodyLimit)
 }
